2669c2ab398cf0eb48cdd286ac05060e75a5341d
acemaster <vivekhtc25@gmail.com>
2015-12-13 15:20:23 +0530

    resolves acemaster/eventapp #5 Added admin api addevent method

--- evm/views.py
+++ evm/views.py
@@ -17,6 +17,7 @@ from django.core.servers.basehttp import FileWrapper
 from django.conf import settings
 from authentication.views import get_client_ip
 from authentication.models import UserProfile
+import notification
 # Create your views here.
 
 
@@ -300,6 +301,70 @@ def addfollowing(request):
 				u.save()
 		response['success']=1
 	return JsonResponse(response)
+
+
+@csrf_exempt
+def addevent(request):
+	"""
+	Add an event if the user is admin
+	=======input===========
+	name
+	type
+	subtype
+	club
+	date_time
+	contact_name_1
+	contact_name_2
+	contact_number_1
+	contact_number_2
+	venue
+	photo -> It is an image that is sent here
+	description
+
+	====Output==========
+	success 0 or 1
+	message -> for toast
+
+	"""
+	response={}
+	response['success']=0
+	if request.method == 'POST':
+		email=request.POST['email']
+		user=User.object.get(username=email)
+		if user.is_superuser:
+			event_name=request.POST['name']
+			event_type=request.POST['type']
+			event_subtype=request.POST['subtype']
+			event_club=request.POST['club']
+			event_date_time=request.POST['date_time']
+			event_contact_name_1=request.POST['contact_name_1']
+			event_contact_name_2=request.POST['contact_name_2']
+			event_contact_number_1=request.POST['contact_number_1']
+			event_contact_number_2=request.POST['contact_number_2']
+			event_venue=request.POST['venue']
+			event_alias=event_name+event_date_time.replace(' ','')+event_venue
+			event_addedby=user
+			event=Event(name=event_name,type=event_type,subtype=event_subtype,club=event_club,date_time=event_date_time,contact_name_1=event_contact_name_1,contact_number_1=event_contact_number_1,contact_name_2=event_contact_name_2,contact_number_2=event_contact_number_2,venue=event_venue,alias=event_alias,addedby=event_addedby)
+			event.save()
+			event_image=request.FILES['photo']
+			event_description=request.POST['description']
+			event_desp=Content(event=event,image=event_image,description=event_description,addedby=event_addedby)
+			event_desp.save()
+			#Sending notification about the new event
+			clubc=Club.objects.get(alias=event_club)
+			ids = UserFollow.objects.values_list('user__userprofile__mobile_id', flat=True).filter(club = clubc)
+			if len(ids) > 0:
+				message="New event "+obj.event.name+" has been added. Check it out"
+				notification.send_notification_custom(obj.event,ids,message)
+			response['message']="Event "+event_name+" has been added"
+			response['success']=1
+		else:
+			response['message']="User is not an admin. Cannot add the event"
+			response['success']=0
+	else:
+		response['message']="Sorry wrong place"
+	return JsonResponse(response)
+
 
 def download(request):
 	if request.user.is_superuser:
